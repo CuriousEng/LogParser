dabb602eec07ec3ccf560f50ade7fc54992a6015
CuriousEngineer <kirill.s.works@gmail.com>
2019-07-24 23:23:23 +0300

    LogParser is implemented.

--- src/com/LogParser.java
+++ src/com/LogParser.java
@@ -1,4 +1,137 @@
 package com;
 
-public class LogParser {
+import com.query.IPQuery;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogParser implements IPQuery {
+    public List<Path> filePathsCollector;
+    public List<String> infoCollector;
+    Path logDir;
+
+
+    public LogParser(Path logDir){
+        this.logDir = logDir;
+        filePathsCollector = this.getPathsList(logDir);
+        infoCollector = this.getInfo(filePathsCollector);
+    }
+
+    private List<Path> getPathsList(Path logDir){
+        List<Path> listOfPaths = new ArrayList<>();
+        try {
+            Stream<Path> pathStream = Files.walk(logDir);
+            pathStream
+                    .filter(Files::isRegularFile)
+                    .filter(x -> x.toFile().getName().endsWith(".log"))
+                    .forEach(listOfPaths::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listOfPaths;
+    }
+
+    private List<String> getInfo (List<Path> listOfPaths){
+        List<String> logInfo = new ArrayList<>();
+        try {
+            for(Path path: listOfPaths) {
+                logInfo.addAll(Files.readAllLines(path));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logInfo;
+    }
+
+    private String getIP(String logLine){
+        return logLine.split("\\t")[0];
+    }
+
+    private String getUser(String logString){
+        return logString.split("\\t")[1];
+    }
+
+    private String getStatus(String s){
+        return s.split("\\t")[4];
+    }
+
+    private String getEvent(String s){
+        return s.split("\\t")[3].split(" ")[0];
+    }
+
+    private Date getDate(String logString){
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        Date date = null;
+        try {
+            return format.parse(logString.split("\\t")[2]);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    //получение из общего списка списка по датам
+    private List<String> getListDate(List<String> logInfo, Date after, Date before){
+        if (after == null && before == null){
+            return logInfo;
+        } else if (after == null) {
+            return logInfo.stream()
+                    .filter(s -> getDate(s).getTime() <= before.getTime())
+                    .collect(Collectors.toList());
+        } else if (before == null) {
+            return logInfo.stream()
+                    .filter(s -> getDate(s).getTime() >= after.getTime())
+                    .collect(Collectors.toList());
+        } else {
+            return logInfo.stream()
+                    .filter(s -> getDate(s).getTime() >= after.getTime() && getDate(s).getTime() <= before.getTime())
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Override
+    public int getNumberOfUniqueIPs(Date after, Date before) {
+        return getUniqueIPs(after, before).size();
+    }
+
+    @Override
+    public Set<String> getUniqueIPs(Date after, Date before) {
+        return getListDate(infoCollector, after, before).stream()
+                .map(s -> s.split("\\t")[0])
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getIPsForUser(String user, Date after, Date before) {
+        return getListDate(infoCollector, after, before).stream()
+                .filter(s -> getUser(s).equals(user))
+                .map(this::getIP)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getIPsForEvent(Event event, Date after, Date before) {
+        return getListDate(infoCollector, after, before).stream()
+                .filter(s -> getEvent(s).equals(event.toString()))
+                .map(this::getIP)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<String> getIPsForStatus(Status status, Date after, Date before) {
+        return getListDate(infoCollector, after, before).stream()
+                .filter(s -> getStatus(s).equals(status.toString()))
+                .map(this::getIP)
+                .collect(Collectors.toSet());
+    }
 }
--- src/com/Solution.java
+++ src/com/Solution.java
@@ -1,11 +1,58 @@
 package com;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("c:/com.logs/"));
-        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+        LogParser logParser = new LogParser(Paths.get("/home/code/src/Projects/LogParser/src/com/logs/"));
+        for(Path p: logParser.filePathsCollector){
+            System.out.println(p.getFileName());
+        }
+        System.out.println();
+        for(String str: logParser.infoCollector){
+            System.out.println(str);
+        }
+        System.out.println();
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        Set<String> res, result;
+        try {
+            Date after = format.parse("30.08.2012 16:08:13");
+            Date before = format.parse("03.01.2014 03:45:23");
+             res = logParser.getUniqueIPs(null, null);
+             for(String s: res){
+                 System.out.println(s);
+             }
+            System.out.println(logParser.getNumberOfUniqueIPs(null, null));
+            System.out.println();
+
+            result = logParser.getIPsForUser("Eduard Petrovich Morozko", null, null);
+            for(String s: result){
+                System.out.println(s);
+            }
+
+            System.out.println();
+
+            result = logParser.getIPsForEvent(Event.SOLVE_TASK, null, null);
+            for(String s: result){
+                System.out.println(s);
+            }
+            System.out.println();
+
+            result = logParser.getIPsForStatus(Status.OK, null, null);
+            for(String s: result){
+                System.out.println(s);
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        //System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
     }
 }
